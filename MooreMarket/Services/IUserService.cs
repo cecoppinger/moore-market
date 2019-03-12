17468e5faeb1225d77387d0ebff1da3716bed060
Clarence Coppinger <thea.trio@gmail.com>
2019-03-11 20:14:51 -0500

    Added password encryption

--- MooreMarket/Services/IUserService.cs
+++ MooreMarket/Services/IUserService.cs
@@ -40,7 +40,7 @@ namespace MooreMarket.Services
         return null;
       }
 
-      if(!ValidatePassword(password, user.Password))
+      if(!VerifyPasswordHash(password, user.Password, user.StoredSalt))
       {
         return null;
       }
@@ -65,15 +65,46 @@ namespace MooreMarket.Services
         throw new AppException("Username already taken");
       }
 
+      byte[] passwordHash, passwordSalt;
+      CreatePasswordHash(password, out passwordHash, out passwordSalt);
+      
+      user.Password = passwordHash;
+      user.StoredSalt = passwordSalt;
+
       _context.Users.Add(user);
       _context.SaveChanges();
 
       return user;
     }
 
-    private bool ValidatePassword(string password, string userPassword)
+    private static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
     {
-      return password == userPassword;
+      if (password == null) throw new ArgumentNullException("password");
+      if (string.IsNullOrWhiteSpace(password)) throw new ArgumentException("Value cannot be empty or whitespace only string.", "password");
+
+      using (var hmac = new System.Security.Cryptography.HMACSHA512())
+      {
+        passwordSalt = hmac.Key;
+        passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+      }
+    }
+    private static bool VerifyPasswordHash(string password, byte[] storedHash, byte[] storedSalt)
+    {
+      if (password == null) throw new ArgumentNullException("password");
+      if (string.IsNullOrWhiteSpace(password)) throw new ArgumentException("Value cannot be empty or whitespace only string.", "password");
+      if (storedHash.Length != 64) throw new ArgumentException("Invalid length of password hash (64 bytes expected).", "passwordHash");
+      if (storedSalt.Length != 128) throw new ArgumentException("Invalid length of password salt (128 bytes expected).", "passwordHash");
+
+      using (var hmac = new System.Security.Cryptography.HMACSHA512(storedSalt))
+      {
+        var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+          if (computedHash[i] != storedHash[i]) return false;
+        }
+      }
+
+      return true;
     }
   }
 }
